9e964276d4a0526dab60abf601112f424e6b82b2
Your Name <your@email.com>
2026-04-24 12:13:47 -0400

    changing name

--- GRSM_MICR_475/scr/visitation visualization.R
+++ GRSM_MICR_475/scr/visitation visualization.R
@@ -7,7 +7,7 @@ visitation <- read.csv("visitation.csv")
 #removing columns to keep the most relevant for this visualization + keeping only 2021-2025
 #also attempting to change Year to character for graph color purpose (does not appear to be working for me)
 reduced_vis <- visitation |>
-  mutate(as.character(visitation$Year)) |>
+  mutate(as_factor(visitation$Year)) |>
   filter(Year > 2020) |>
   select(c(2,3,4,5,8))
 
@@ -33,5 +33,5 @@ Q2 <- ggplot(data=reduced_vis, aes(x=Year, y=AnnualTotal, fill=Year)) +
 
 #How has visitation changed in the past 5 years in monthly visitation? 
 Q2A <- ggplot(data=reduced_vis, aes(x=factor(month, levels=month_order), y=attendance, fill=Year)) +
-  geom_bar(position="stack", stat="identity") +
+  geom_bar(position="dodge", stat="identity") +
   labs(title = "Monthly Visitation per Year", x = "Month", y = "Attendance")
